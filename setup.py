5ba6e4837c7d96d6ca6c9e19671daea195456750
Adrian Rivas <adrian_rvzz@hotmail.com>
2024-11-18 01:30:47 -0700

    Project init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bank_parser_py",
+    version="0.1.0",
+    author="Adrian Rivas",
+    description="Preprocessing python program for invoices",
+    url="https://github.com/AdrianRvzz/bank_parser_py",
+)
