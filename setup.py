be53d1929c97a31d31402f3ad96aecafbee5e38f
Adrian Rivas <adrian_rvzz@hotmail.com>
2025-04-10 12:17:49 -0600

    Change version within updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bank_parser_py",
-    version="0.1.0",
+    version="0.3.0",
     author="Adrian Rivas",
     description="Preprocessing python program for invoices",
     url="https://github.com/AdrianRvzz/bank_parser_py",
